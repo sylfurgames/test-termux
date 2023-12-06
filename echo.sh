844b029e8de28a58af044d831a889710cab5e785
sylfurgames <123963378+sylfurgames@users.noreply.github.com>
2023-12-06 19:49:16 +0300

    echo.sh

--- echo.sh
+++ echo.sh
@@ -1,5 +1,31 @@
-echo by starfall
-sleep 1
-echo test
-command == "a"
-echo lox
+#!/bin/bash
+# Функция для команды help
+help_command() {
+  echo "Доступные команды:"
+  echo "help - показывает список доступных команд"
+  echo "author - показывает информацию об авторе"
+}
+# Функция для команды author
+author_command() {
+  echo "Автор: starfall"
+  echo "Контактная информация: @starfal8k"
+}
+# Главный цикл программы
+while true; do
+  # Выводим приглашение для ввода команды
+  echo -n "Введите команду (help для помощи): "
+  read command
+  # Проверяем введенную команду
+  case $command in
+    help)
+      help_command
+      ;;
+    author)
+      author_command
+      ;;
+    *)
+      echo "Некорректная команда. Введите 'help' для помощи."
+      ;;
+  esac
+  echo # Добавляем пустую строку для отделения вывода команды от приглашения
+done
