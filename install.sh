e89bde4f092c4d799a65381ffcaefaa78f6258cc
sylfurgames <123963378+sylfurgames@users.noreply.github.com>
2023-12-06 05:08:08 +0300

    install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+pathFile="sylfurgames" 
+cd ~/../usr/bin 
+# команда
+touch sylfurgames
+echo "cd ~/$pathFile/ && python echo.sh" >  sylfurgames
+chmod +x sylfurgames
+cd ~/
+#конец
