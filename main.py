5cef9f212810a6377c171a23cd08b673eae1305a
sylfurgames <123963378+sylfurgames@users.noreply.github.com>
2023-12-10 02:33:29 +0300

    main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-import os
-try:
-	import colorama
-except:
-	os.system("pip install colorama")
-	import colorama
-title = """██░└┐░░░░░░░░░░░░░░░░░┌┘░████░└┐░░░░░░░░░░░░░░░░░┌┘░██
-██░░└┐░░░░░░░░░░░░░░░┌┘░░████░░└┐░░░░░░░░░░░░░░░┌┘░░██
-██░░┌┘▄▄▄▄▄░░░░░▄▄▄▄▄└┐░░████░░┌┘▄▄▄▄▄░░░░░▄▄▄▄▄└┐░░██
-██▌░│██████▌░░░▐██████│░▐████▌░│██████▌░░░▐██████│░▐██
-███░│▐███▀▀░░▄░░▀▀███▌│░██████░│▐███▀▀░░▄░░▀▀███▌│░███
-██▀─┘░░░░░░░▐█▌░░░░░░░└─▀████▀─┘░░░░░░░▐█▌░░░░░░░└─▀██
-██▄░░░▄▄▄▓░░▀█▀░░▓▄▄▄░░░▄████▄░░░▄▄▄▓░░▀█▀░░▓▄▄▄░░░▄██
-████▄─┘██▌░░░░░░░▐██└─▄████████▄─┘██▌░░░░░░░▐██└─▄████
-█████░░▐█─┬┬┬┬┬┬┬─█▌░░██████████░░▐█─┬┬┬┬┬┬┬─█▌░░█████
-████▌░░░▀┬┼┼┼┼┼┼┼┬▀░░░▐████████▌░░░▀┬┼┼┼┼┼┼┼┬▀░░░▐████
-█████▄░░░└┴┴┴┴┴┴┴┘░░░▄██████████▄░░░└┴┴┴┴┴┴┴┘░░░▄█████"""
-while True:
-	os.system("clear")
-	print(colorama.Fore.RED + colorama.Back.GREEN)
+#!/usr/bin/env python
+import sys
+def help_command():
+    print("--Доступные команды--")
+    print("help - Показывает доступные команды")
+    print("author - Показывает автора утилиты")
+def author_command():
+    print("Автор: starfall")
+if __name__ == "__main__":
+    while True:
+        command = input("》 ")
+        if command == "help":
+            help_command()
+        elif command == "author":
+            author_command()
+        elif command == "stop":
+            print("Stopping the script...")
+            break
+        else:
+            print("Unknown command. Use 'help' command to see available options.")
